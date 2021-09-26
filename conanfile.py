a25c5d063367b3da663090abfa91a260ba069b8b
Khalil Estell <kammce@google.com>
2021-09-26 09:15:36 -0700

    Remove unneeded imports from conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,4 @@
 from conans import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake
-from conan.tools.layout import cmake_layout
 
 
 class libarmcortex_conan(ConanFile):
@@ -13,8 +11,6 @@ class libarmcortex_conan(ConanFile):
     topics = ("peripherals", "hardware")
     exports_sources = "CMakeLists.txt", "libarmcortex/*"
     no_copy_source = True
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package_multi"
 
     def package(self):
         self.copy("*.hpp")
