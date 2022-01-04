57f73da9e1993ab3ed8383fd968d97e2a5a47950
Khalil Estell <kammcecorp@gmail.com>
2022-01-03 20:03:51 -0800

    Add comment docs to interrupt.hpp

--- include/libarmcortex/system_timer.hpp
+++ include/libarmcortex/system_timer.hpp
@@ -5,25 +5,25 @@
 #include "interrupt.hpp"
 
 #include <libembeddedhal/config.hpp>
+#include <libembeddedhal/timer/timer.hpp>
 
 namespace embed::cortex_m {
-
-/// @brief  Structure type to access the System Timer (SysTick).
-struct system_tick_t
-{
-  /// Offset: 0x000 (R/W)  SysTick Control and Status Register
-  volatile uint32_t ctrl;
-  /// Offset: 0x004 (R/W)  SysTick Reload Value Register
-  volatile uint32_t load;
-  /// Offset: 0x008 (R/W)  SysTick Current Value Register
-  volatile uint32_t val;
-  /// Offset: 0x00C (R/ )  SysTick Calibration Register
-  const volatile uint32_t calib;
-};
-
-class system_timer
+class system_timer : public embed::timer
 {
 public:
+  /// @brief  Structure type to access the System Timer (SysTick).
+  struct registers
+  {
+    /// Offset: 0x000 (R/W)  SysTick Control and Status Register
+    volatile uint32_t ctrl;
+    /// Offset: 0x004 (R/W)  SysTick Reload Value Register
+    volatile uint32_t load;
+    /// Offset: 0x008 (R/W)  SysTick Current Value Register
+    volatile uint32_t val;
+    /// Offset: 0x00C (R/ )  SysTick Calibration Register
+    const volatile uint32_t calib;
+  };
+
   /// Enumeration holding the bit positions of used flags.
   /// Source: "UM10562 LPC408x/407x User manual" table 83 page 132
   enum control_bitmap : uint8_t
@@ -38,12 +38,12 @@ public:
   static constexpr int irq = -1;
 
   /// Address of the ARM Cortex SysTick peripheral.
-  inline static auto* system_tick = reinterpret_cast<system_tick_t*>(address);
+  inline static auto* system_tick = reinterpret_cast<registers*>(address);
 
   static void setup_for_unittesting()
   {
     // Dummy registers for unit testing
-    static system_tick_t dummy_system_tick{};
+    static registers dummy_system_tick{};
 
     // Replace the address of the peripheral pointer with the dummy structure so
     // that they can be inspected during unit tests.
@@ -56,6 +56,19 @@ public:
       setup_for_unittesting();
     }
   }
+
+   boost::leaf::result<bool> is_running() override {
+
+   }
+   boost::leaf::result<void> control(controls p_control) override {
+
+   }
+   boost::leaf::result<void> attach_interrupt(
+    std::function<void(void)> p_callback,
+    std::chrono::nanoseconds p_interval,
+    type p_type = type::continuous) override {
+
+    }
 
   void attach_interrupt(interrupt_pointer p_system_tick_handler)
   {
