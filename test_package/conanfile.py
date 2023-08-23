8a71cdf5e04883fb4ef94bccb35ad7e1b4de0706
Khalil Estell <kammce@google.com>
2023-08-23 07:14:36 -0700

    :arrow_up: Upgrade to libhal/2.0.1

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -26,6 +26,9 @@ class TestPackageConan(ConanFile):
     def _bare_metal(self):
         return self.settings.os == "baremetal"
 
+    def build_requirements(self):
+        self.tool_requires("cmake/3.27.1")
+
     def requirements(self):
         self.requires(self.tested_reference_str)
 
